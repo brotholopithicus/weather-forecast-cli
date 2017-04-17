a77a18368f1ba89e0b268beabc01ce9f72085f72
James Jarvis <jamespjarvis@gmail.com>
2017-04-17 08:19:59 -0400

    removed extraneous

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const parseData = require('./parse');
-const requestify = require('./requestify');
+const requestify = require('node-requestify');
 const url = `http://api.openweathermap.org/data/2.5/forecast?zip=01451,us&mode=json&appid=46c7bc73ac6a9b519b290f7cc313fac1`;
 
 async function getData() {
